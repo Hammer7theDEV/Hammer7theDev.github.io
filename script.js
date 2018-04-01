64fbc62621763b5f1a7ba0a4c32f68ff91379bb4
rioforce <rioforce@users.noreply.github.com>
2018-04-01 19:12:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-"use strict";
-const qForeground = document.querySelector("#foreground");
-const colors = ["color-red", "color-green", "color-blue", "color-yellow", "color-orange", "color-pink", "color-black", "color-white"];
+(function() {
+  "use strict";
+  var qForeground = document.querySelector("#foreground");
+  var colors = ["color-red", "color-green", "color-blue", "color-yellow", "color-orange", "color-pink", "color-black", "color-white"];
 
-function pickColorAtRandom() {
-  const newColor = colors[Math.floor(Math.random() * colors.length)];
-  
-  // Remove previous colors
-  if (/color-/.test(qForeground.className)) {
-    const oldClass = qForeground.className.match(/color-\w+/);
-    qForeground.classList.remove(oldClass);
+  function pickColorAtRandom() {
+    var newColor = colors[Math.floor(Math.random() * colors.length)];
+
+    // Remove previous colors
+    if (/color-/.test(qForeground.className)) {
+      var oldClass = qForeground.className.match(/color-\w+/);
+      qForeground.classList.remove(oldClass);
+    }
+    qForeground.classList.add(newColor);
   }
-  qForeground.classList.add(newColor);
-}
 
-setInterval(pickColorAtRandom, 200);
+  setInterval(pickColorAtRandom, 200);
+}());
